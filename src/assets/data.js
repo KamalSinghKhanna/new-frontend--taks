e548ec2f8cbd52c17478b8dad56bdb8214fb6288
darkillusior <94616975+darkillusior@users.noreply.github.com>
2024-01-17 13:07:44 +0530

    Update data.js

--- src/assets/data.js
+++ src/assets/data.js
@@ -81,7 +81,7 @@ export const features = [
     title: "Reach Out",
     description:
       "Start by reaching out to us. Contact us through our website or drop us an email. Share your project details, expectations, and requirements. We're here to listen and understand your vision.",
-    bgColor: "bg-gradient-to-r from-green-300 via-blue-500 to-purple-600",
+    bgColor: "bg-gradient-to-r from-indigo-300 to-purple-400",
   },
 
   {
@@ -89,39 +89,39 @@ export const features = [
     description:
       "We'll schedule a consultation to dive deeper into your project. This is where we discuss your goals, preferences, and any specific features you have in mind. Our team will work closely with you to ensure a clear understanding of your project requirements.",
     bgColor:
-      "bg-[conic-gradient(at_right,_var(--tw-gradient-stops))] from-yellow-200 via-pink-200 to-pink-400",
+      "bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-sky-400 to-blue-800",
   },
   {
     title: "Proposal & Agreement",
     description:
       "Based on our discussions, we'll provide you with a detailed proposal outlining the scope of work, timelines, and costs. Once we reach an agreement, we proceed to the next steps.",
 
-    bgColor: "bg-gradient-to-r from-red-400 via-gray-300 to-blue-500",
+    bgColor: "bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-sky-400 to-indigo-900",
   },
   {
     title: "Development",
     description:
       "Our team of skilled developers will bring your vision to life. Throughout the development process, we'll keep you updated, ensuring transparency and giving you the opportunity to provide feedback.",
-    bgColor: "bg-gradient-to-r from-purple-200 via-purple-400 to-purple-800",
+    bgColor: "bg-gradient-to-bl from-indigo-900 via-indigo-400 to-indigo-900
   },
   {
     title: "Refinement",
     description:
       "Your satisfaction is paramount. We welcome your feedback and offer a revision process to make any necessary adjustments. Our goal is to refine the project until it aligns perfectly with your expectations.",
-    bgColor: "bg-gradient-to-r from-green-200 via-green-300 to-blue-500",
+    bgColor: "bg-[conic-gradient(at_right,_var(--tw-gradient-stops))] from-indigo-200 via-slate-600 to-indigo-200",
   },
   {
     title: "Delivery",
     description:
       "Once the project meets your approval, we'll deliver the final product. Our team ensures a smooth handover, providing any necessary documentation and support.",
     bgColor:
-      "bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-yellow-500 via-purple-500 to-blue-500",
+      "bg-gradient-to-r from-rose-500 via-red-400 to-red-500",
   },
   {
     title: "Ongoing Support",
     description:
       "Our commitment doesn't end with delivery. We offer ongoing support, ensuring that your website continues to meet your evolving needs. Whether it's updates, maintenance, or additional features, we're here for you.",
-    bgColor: "bg-gradient-to-r from-sky-400 via-rose-400 to-lime-400",
+    bgColor: "bg-gradient-to-r from-cyan-200 to-cyan-400",
   },
   {
     title: "Ready to bring your ideas to life?",
